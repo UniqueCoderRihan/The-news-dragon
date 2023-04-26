5ad32d0b80f342686ac63faf323f0139b4f16be3
Rihanul Islam <rihanthedev@gmail.com>
2023-04-26 15:09:34 +0600

    Catagoris Data Recived

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const LeftNav = () => {
+
+    const [catagoris,setCatagoris]= useState([]);
+    console.log(catagoris);
+    useEffect(()=>{
+        fetch('http://localhost:5000/catagoris')
+        .then(res=>res.json())
+        .then(data=>setCatagoris(data))
+        .catch(error=>console.log(error))
+    },[])
+    
     return (
         <div>
-            <h3>This is Left</h3>
+            
         </div>
     );
 };
