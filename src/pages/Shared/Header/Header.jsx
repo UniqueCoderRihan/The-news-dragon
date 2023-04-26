1798f5f95b933a66a0900fde6245066da935ac4a
Rihanul Islam <rihanthedev@gmail.com>
2023-04-26 13:25:02 +0600

    header

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment/moment';
 import Button from 'react-bootstrap/Button';
-import { Container } from 'react-bootstrap';
+import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import Marquee from 'react-fast-marquee';
 const Header = () => {
     return (
@@ -14,10 +14,30 @@ const Header = () => {
             </div>
             <div className='d-flex'>
                 <Button variant="danger">Tranding</Button>{' '}
-                <Marquee>
+                <Marquee pauseOnHover={true} speed={50} className='text-danger'>
                     Hello World Iam Here ,Tomi eikhane Keno ascho? News Site Na Eita
                 </Marquee>
             </div>
+            {/* Nabvar section started */}
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#features">About</Nav.Link>
+                            <Nav.Link href="#pricing">Carrer</Nav.Link>
+                            
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Profile</Nav.Link>
+                            <Nav.Link eventKey={2} href="">
+                            <Button variant="secondary">Login</Button>{' '}
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </Container>
     );
 };
