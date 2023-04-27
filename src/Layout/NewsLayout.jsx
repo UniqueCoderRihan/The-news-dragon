e8e87ddab2aadb52b4e4f82fddc1a3260d122a31
Rihanul Islam <rihanthedev@gmail.com>
2023-04-27 07:35:10 +0600

    News Layout

--- src/Layout/NewsLayout.jsx
+++ src/Layout/NewsLayout.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import Header from '../pages/Shared/Header/Header'
 import Footer from '../pages/Shared/Footer/Footer'
 import { Col, Container, Row } from 'react-bootstrap';
-import LeftNav from '../pages/Shared/LeftNav/LeftNav';
 import RightNav from '../pages/Shared/RightNav/RightNav';
 import { Outlet } from 'react-router-dom';
 
-const Main = () => {
+const NewsLayout = () => {
     return (
         <Container>
             <Header></Header>
             <Row>
-                <Col lg={3}> <LeftNav></LeftNav> </Col>
-                <Col lg={6}> <Outlet></Outlet> </Col>
+                
+                <Col lg={9}> <Outlet></Outlet> </Col>
                 <Col lg={3}> <RightNav></RightNav> </Col>
             </Row>
             <Footer></Footer>
@@ -20,4 +19,4 @@ const Main = () => {
     );
 };
 
-export default Main;
+export default NewsLayout;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from '../Layout/Main'
 import Home from '../pages/Home/Home/Home'
+import Category from "../pages/Shared/Category/Category";
+import NewsLayout from "../Layout/NewsLayout";
+import News from "../pages/News/News";
 const router = createBrowserRouter([
     {
         path:'/',
@@ -9,6 +12,20 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path:'/category/:id',
+                element: <Category></Category>
+            }
+        ]
+    },
+    {
+        path:'news',
+        element: <NewsLayout></NewsLayout>,
+        children:[
+            {
+                path: ':id',
+                element: <News></News>
             }
         ]
     }
