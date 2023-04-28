0bfb8e8c4bfc56fc69da6b4f3f0ca46e6aab3290
Rihanul Islam <rihanthedev@gmail.com>
2023-04-28 14:25:46 +0600

    LeftNavDone

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import frist from '../../../assets/1.png'
+import secound from '../../../assets/2.png'
+import thrid from '../../../assets/3.png'
 
 const LeftNav = () => {
 
@@ -25,6 +29,42 @@ const LeftNav = () => {
                         </p>)
                 }
             </div>
+            <Row xs={1} md={1} lg={1} className="g-4 ">
+
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src={frist} />
+                        <Card.Body>
+                            <Card.Title>Tucker Carlson Swears Vladimir Putin</Card.Title>
+                            <Card.Text>
+                            Joe Biden, Military, News, Russia, Security, UK, Ukraine, United States,
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src={secound} />
+                        <Card.Body>
+                            <Card.Title>Tucker Carlson Swears Vladimir Putin</Card.Title>
+                            <Card.Text>
+                            Joe Biden, Military, News, Russia, Security, UK, Ukraine, United States,
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src={thrid} />
+                        <Card.Body>
+                            <Card.Title>Tucker Carlson Swears Vladimir Putin</Card.Title>
+                            <Card.Text>
+                            Joe Biden, Military, News, Russia, Security, UK, Ukraine, United States,
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
         </div>
     );
 };
