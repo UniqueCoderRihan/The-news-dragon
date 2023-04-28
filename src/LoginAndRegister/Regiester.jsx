11f6a10a4433a6cf6e0a445fc061194fa3ebde63
Rihanul Islam <rihanthedev@gmail.com>
2023-04-28 15:47:02 +0600

    Login & Register Routes

---
+++ src/LoginAndRegister/Regiester.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Regiester = () => {
+    return (
+        <div>
+            <h2>This Is Resifvsjkf</h2>
+        </div>
+    );
+};
+
+export default Regiester;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,21 +1,37 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Main from '../Layout/Main'
 import Home from '../pages/Home/Home/Home'
 import Category from "../pages/Shared/Category/Category";
 import NewsLayout from "../Layout/NewsLayout";
 import News from "../pages/News/News";
+import LoginLayout from "../Layout/LoginLayout";
+import Login from "../LoginAndRegister/Login";
+import Regiester from "../LoginAndRegister/Regiester";
 const router = createBrowserRouter([
     {
-        path:'/',
+        path: '/',
+        element: <LoginLayout></LoginLayout>,
+        children:[
+            {
+                path: '/',
+                element: <Navigate to='/category/0'></Navigate>
+            },
+            {
+                path:'/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Regiester></Regiester>
+            }
+        ]
+    },
+    {
+        path:'/category',
         element: <Main></Main>,
         children: [
             {
-                path: '/',
-                element: <Category></Category>,
-                loader: ()=>fetch('http://localhost:5000/news')
-            },
-            {
-                path:'/category/:id',
+                path:':id',
                 element: <Category></Category>,
                 loader:({params})=> fetch(`http://localhost:5000/catagoris/${params.id}`)
                 // Always Note Your Link Will Be Server Site. I was depress 40 minit for this Problem
