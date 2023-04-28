fc77ec2398a791658d4f7cdae88eb7774460a9cc
Rihanul Islam <rihanthedev@gmail.com>
2023-04-28 12:46:56 +0600

    Catagory items added

--- src/pages/Home/CategoryNews/CategoryNews.jsx
+++ src/pages/Home/CategoryNews/CategoryNews.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const CategoryNews = ({news}) => {
-    const {title}=news;
+    const { title,_id,total_view,details,image_url } = news;
     return (
-        <div className='border' >
-           <h3>{title}</h3> 
-        </div>
+        <Card className="mb-4">
+            <Card.Header>Featured</Card.Header>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Text>
+                    {details.length< 250? details : <>{details.slice(0,250)}... <Link to={`/news/${_id}`}> ReadMore...</Link> </>}
+                </Card.Text>
+            </Card.Body>
+            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+        </Card>
     );
 };
 
