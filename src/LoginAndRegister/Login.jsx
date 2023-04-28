4b2c62b0b60122ef6b4d6a37b7df26de035813d5
Rihanul Islam <rihanthedev@gmail.com>
2023-04-28 16:02:35 +0600

    Login and Register done

--- src/LoginAndRegister/Login.jsx
+++ src/LoginAndRegister/Login.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Button, Container,Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h2>This Is Login Frorm</h2>
-        </div>
+        <Container className='w-25 mx-auto'>
+            <h3>Please Login</h3>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' required placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+                <p><small> <Link to='/register'>New To? SIngUp</Link></small></p>
+            </Form>
+        </Container>
     );
 };
 
--- src/LoginAndRegister/Regiester.jsx
+++ src/LoginAndRegister/Regiester.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Regiester = () => {
     return (
-        <div>
-            <h2>This Is Resifvsjkf</h2>
-        </div>
+        <Container className='w-25 mx-auto'>
+            <h3>Please Register</h3>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Enter Your Name: </Form.Label>
+                    <Form.Control type="text" name='name' placeholder="Enter Name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>PHOTO URL </Form.Label>
+                    <Form.Control type="text" name='photo' placeholder="Enter Photo Url" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' required placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="AMi Raji Achi Sob Khane" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+                <p><small> <Link to='/login'>Alredy Have an Account? Login</Link></small></p>
+            </Form>
+        </Container>
     );
 };
 
