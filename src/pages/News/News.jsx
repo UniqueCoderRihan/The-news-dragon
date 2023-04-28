bbac14bc7aa744f4c65aa9e776247dbceaa023ee
Rihanul Islam <rihanthedev@gmail.com>
2023-04-28 13:56:33 +0600

    Details Done

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const News = () => {
+    const news = useLoaderData();
+    const { title, _id, total_view, details, image_url,category_id, author, rating } = news;
     return (
-        <div>
-            <h3>More Details Comming Sooooon</h3>
-        </div>
+        <Card>
+            <Card.Img variant="top" src={image_url} />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                    {details}
+                </Card.Text>
+                <Link to={`/category/${category_id}`}><Button variant="danger"> <FaArrowLeft></FaArrowLeft> Go To This Catagory</Button></Link>
+            </Card.Body>
+        </Card>
     );
 };
 
